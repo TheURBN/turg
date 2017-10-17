a2bc15cb81448728625f2b75f05e6360d770045f
Anton Larkin <alarkin@odin.com>
2017-10-17 15:45:58 +0300

    Change ws API

--- turg/views/websocket.py
+++ turg/views/websocket.py
@@ -49,28 +49,30 @@ def factory(app):
 
 
 async def process_request(data, ws, app):
-    if 'method' not in data or ('args' not in data or not isinstance(data, dict)):
+    if 'type' not in data or ('args' not in data or not isinstance(data, dict)):
         return {'error': {'message': {'Method and args required'}}}
 
-    method = data['method'].lower()
+    _id = data.get('id', None)
+    _type = data['type'].lower()
     args = data['args']
+    meta = {'id': _id, 'type': _type}
 
-    if method == 'get':
-        await retrieve(args, ws, app)
-    elif method == 'post':
-        await place(args, ws, app)
+    if _type == 'range':
+        await retrieve(args, ws, app, meta)
+    elif _type == 'update':
+        await place(args, ws, app, meta)
     else:
         await ws.send_json({'error': {'message': {'Unknown method or no method specified'}}})
 
 
-async def retrieve(args, ws, app):
+async def retrieve(args, ws, app, meta):
     x, y, r = args.get('x', 0), args.get('y', 0), args.get('range', 25)
     voxels = await get_voxels(x, y, r, app['db'])
     app['players'][ws] = {'x': x, 'y': y, 'range': r}
-    await ws.send_json(voxels)
+    await ws.send_json({'data': voxels, 'meta': meta})
 
 
-async def place(args, ws, app):
+async def place(args, ws, app, meta):
     if not verify_payload(args):
         return await ws.send_json({'error': {'message': 'Invalid payload'}})
 
@@ -81,15 +83,15 @@ async def place(args, ws, app):
     except ValueError as e:
         return await ws.send_json({'error': {'message': str(e)}})
     else:
-        return await broadcast(voxel, app)
+        return await broadcast(voxel, app, meta)
 
 
-async def broadcast(voxel, app):
+async def broadcast(voxel, app, meta):
     for ws in app['websockets']:
         position = app['players'].get(ws)
         if not position or in_range(voxel, position):
             try:
-                await ws.send_json(attr.asdict(voxel))
+                await ws.send_json({'data': attr.asdict(voxel), 'meta': meta})
             except:
                 logger.info("Failed to send update to socket %s", id(ws))
 
