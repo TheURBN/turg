fe52eda9815653ca227b4ab886eec5b18573e14e
akhaerov <akhaerov@odin.com>
2017-10-22 16:32:45 +0300

    Add new spots and init script

---
+++ tools/init_world.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python3 qbplotter.py --src ./samples/spotA.qb --owner 9  --x 505 --y 505 --sleep 5
+python3 qbplotter.py --src ./samples/spotB.qb --owner 9  --x 550 --y 550 --sleep 5
+python3 qbplotter.py --src ./samples/spotC.qb --owner 9  --x 450 --y 460 --sleep 5
+python3 qbplotter.py --src ./samples/spotD.qb --owner 9  --x 450 --y 550 --sleep 5
+python3 qbplotter.py --src ./samples/spotE.qb --owner 9  --x 550 --y 450 --sleep 5
