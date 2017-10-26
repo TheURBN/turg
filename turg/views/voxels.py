c62aa253442fafc1fe2214c54f83af523f032a79
Max Belov <mbelov@odin.com>
2017-10-26 10:01:55 +0300

    Broadcast on HTTP REST voxel placement

--- turg/views/voxels.py
+++ turg/views/voxels.py
@@ -3,6 +3,7 @@ from aiohttp import web
 from turg.config import Config
 from turg.logger import getLogger
 from turg.models import Voxel, get_voxels, store_voxel, verify_payload
+from turg.views.websocket import broadcast
 
 logger = getLogger()
 config = Config()
@@ -34,10 +35,11 @@ class Voxels(web.View):
         db = self.request.app['db']
 
         try:
-            await store_voxel(voxel, db)
+            voxel = await store_voxel(voxel, db)
         except ValueError:
             return web.json_response({'error': {'message': 'Invalid voxel location'}}, status=409)
         else:
+            await broadcast(voxel, self.request.app, {'id': None, 'type': 'update'})
             return web.json_response({'status': 'ok'}, status=200)
 
 
